bc86053ce8a5b4079c789b5f9d4386037949a049
Nathan Wendt <nawendt@ou.edu>
2016-01-14 16:32:56 -0600

    Start work on grib_ls

--- R/rGRIB.R
+++ R/rGRIB.R
@@ -1,3 +1,20 @@
+#######################
+#     GLOBAL VARS
+#######################
+gribFilterList = list(
+  none      = 0,
+  readOnly  = bitwShiftL(1,0),
+  optional  = bitwShiftL(1,1),
+  edition   = bitwShiftL(1,2),
+  coded     = bitwShiftL(1,3),
+  computed  = bitwShiftL(1,4),
+  duplicate = bitwShiftL(1,5),
+  func      = bitwShiftL(1,6)
+)
+
+#######################
+#    MAIN FUNCTIONS
+#######################
 #' @export
 #' @useDynLib rGRIB rgrib_open
 
@@ -28,16 +45,37 @@ grib_close <- function(gribObj) {
     if (!is.null.externalptr(gribObj$gribHandle)) {
       return(invisible(.Call("rgrib_close",gribObj$gribHandle)))
     } else {
-      stop("GRIB object is already closed")
+      stop("GRIB object is closed or unavailable")
     }
   } else {
     stop("Object is not of class 'grib'")
   }
 }
 
-#
-# PRIVATE FUNCTIONS
-#
+#' @export
+#' @useDynLib rGRIB rgrib_ls
+grib_ls <- function(gribObj,filter="none",nameSpace="") {
+  if (is.grib(gribObj)) {
+    if (!is.null.externalptr(gribObj$gribHandle)) {
+      if (is.null(nameSpace)) {
+        # the c function that uses this parameter
+        # will accept the null string in order to
+        # print all keys, allow NULL to be input
+        # as well
+        nameSpace <- ""
+      }
+      .Call("rgrib_ls",gribObj$gribHandle,as.integer(gribFilterList[filter]),nameSpace)
+    } else {
+      stop("GRIB object is closed or unavailable")
+    }
+  } else {
+    stop("Object is not of class 'grib'")
+  }
+}
+
+#######################
+#    PRIVATE FUNCS
+#######################
 
 is.grib <- function(obj) {
   if (class(obj) == "grib") {
