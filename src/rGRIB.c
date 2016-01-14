ea31a7bc453c4c15c122db7082224fcd879171d1
Nathan Wendt <nwendt@casestudy4.spc.noaa.gov>
2016-01-14 16:32:56 -0600

    Start work on grib_ls

--- src/rGRIB.c
+++ src/rGRIB.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <grib_api.h>
 
 #include <R.h>
 #include <Rinternals.h>
+
+#define MAX_KEY_LEN  255
+#define MAX_VAL_LEN  1024
 
 static void handleFinalizer(SEXP ptr);
 
@@ -24,12 +28,14 @@ SEXP rgrib_open(SEXP R_fileName, SEXP R_mode) {
   input = fopen(p_fileName,p_mode);
 
   if(input == NULL) {
-    error("rGRIB: rgrib_open: unable to open file %s\n",p_fileName);
+    //error("%s(%d): unable to open file %s",__FILE__,__LINE__,p_fileName);
+    error("Unable to open file %s",p_fileName);
   }
 
   h = grib_handle_new_from_file(0,input,&err);
   if (h == NULL) {
-    error("rGRIB: rgrib_open: unable to create handle from file %s\n",p_fileName);
+    //error("%s(%d): unable to create handle from file %s",__FILE__,__LINE__,p_fileName);
+    error("Unable to create handle from file %s",p_fileName);
   }
   fclose(input);
 
@@ -44,6 +50,37 @@ SEXP rgrib_open(SEXP R_fileName, SEXP R_mode) {
  * OPERATIONS
  */
 
+SEXP rgrib_ls(SEXP R_gribHandle, SEXP R_filter, SEXP R_nameSpace){
+  grib_handle *h;
+  grib_keys_iterator *keyIter;
+
+  char value[MAX_VAL_LEN];
+  size_t vlen=MAX_VAL_LEN;
+  int filter = INTEGER(R_filter)[0];
+  const char *p_nameSpace = CHAR(STRING_ELT(R_nameSpace,0));
+
+  h = R_ExternalPtrAddr(R_gribHandle);
+  if (h == NULL) {
+    //error("%s(%d): error with gribHandle input",__FILE__,__LINE__);
+    error("Error with gribHandle input");
+  } else {
+    keyIter=grib_keys_iterator_new(h,filter,p_nameSpace);
+    if (keyIter == NULL) {
+      //error("%s(%d): Unable to create keys iterator",__FILE__,__LINE__);
+      error("Unable to create keys iterator");
+    }
+
+    while(grib_keys_iterator_next(keyIter))
+    {
+      const char* name = grib_keys_iterator_get_name(keyIter);
+      vlen=MAX_VAL_LEN;
+      bzero(value,vlen);
+      GRIB_CHECK(grib_get_string(h,name,value,&vlen),name);
+      Rprintf("%s = %s\n",name,value);
+    }
+  }
+  return R_NilValue;
+}
 
 
 /*
@@ -56,14 +93,17 @@ SEXP rgrib_close(SEXP R_gribHandle) {
   h = R_ExternalPtrAddr(R_gribHandle);
 
   if (h == NULL) {
-    warning("rGRIB: rgrib_close: grib handle already closed");
+    warning("grib handle already closed");
   } else {
     grib_handle_delete(h);
     R_ClearExternalPtr(R_gribHandle);
   }
-
   return R_NilValue;
 }
+
+/*
+ * HELPER FUNCTIONS
+ */
 
 static void handleFinalizer(SEXP ptr)
 {
@@ -74,3 +114,4 @@ static void handleFinalizer(SEXP ptr)
 SEXP isNullPointer(SEXP R_ptr) {
   return ScalarLogical(!R_ExternalPtrAddr(R_ptr));
 }
+
