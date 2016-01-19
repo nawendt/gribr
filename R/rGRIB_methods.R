88d68ad94674c48dae52cc9e1b8b79c0e7e92b7b
Nathan Wendt <nawendt@ou.edu>
2016-01-18 21:41:19 -0600

    Testing on producing R lists in C

--- R/rGRIB_methods.R
+++ R/rGRIB_methods.R
@@ -1,5 +1,12 @@
 #' @export
 #' @useDynLib rGRIB R_grib_length
 length.grib <- function(x) {
-  .Call("R_grib_length",x$handle)
+  .Call("R_grib_length", x$handle)
+}
+
+#' @export
+#' @useDynLib rGRIB R_grib_length
+print.grib <- function(x, ...) {
+  cat("GRIB FILE: ", x$file, "\n")
+  cat("Total Messages: ", .Call("R_grib_length", x$handle), "\n")
 }
