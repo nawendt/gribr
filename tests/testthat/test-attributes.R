a486e2cdfcb2d89663c342db1677d6155a44f8e0
Nathan Wendt <nawendt@ou.edu>
2016-04-29 07:10:31 -0500

    More tests added; grib_select bug found

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -10,6 +10,19 @@ test_that("GRIB file attributes are correct", {
   # length (with multi-message support)
   expect_equal(grib_length(mmg), 21)
 
+  # class
+  expect_s3_class(g, "grib")
+
+  # list contents
+  contents <- grib_list(g)
+  expect_equal(length(contents), 64)
+  expect_type(contents, "character")
+
+  # closing
+  capture_output(grib_close(g))
+  capture_output(grib_close(mmg))
+  expect_equal(is.null.externalptr(g$handle), TRUE)
+
 })
 
 
--- tests/testthat/test-messages.R
+++ tests/testthat/test-messages.R
@@ -1,4 +1,4 @@
-context("GRIB message retrieval")
+context("GRIB message retrieval and attributes")
 
 test_that("Incorrect message requests are handled correctly", {
   g <- grib_open(system.file("extdata", "lfpw.grib1", package = "gribr"))
@@ -8,13 +8,20 @@ test_that("Incorrect message requests are handled correctly", {
   expect_error(grib_get_message(g,0), "out of bounds")
   expect_error(grib_get_message(g,999), "out of bounds")
   gm <- grib_get_message(g,1)
-  expect_is(gm, "gribMessage")
+  expect_s3_class(gm, "gribMessage")
+  gm <- grib_get_message(g, c(1,2))
+  expect_equal(length(gm), 2)
+  expect_equal(all(sapply(gm, class) == "gribMessage"), TRUE)
+
 
   # grib_select
   expect_error(grib_select(g, list(dne = "dne")), "not found")
   expect_error(grib_select(g, list(shortName = "dne")), "no messages matched")
   expect_error(grib_select(g, list()), "keyPairs input")
   expect_error(grib_select(g, c(dne = "dne")), "must be a list")
+  gm <- grib_select(g, list(shortName = c("2t", "2d")))
+  expect_equal(length(gm), 2) # fails due to previous test
+  expect_equal(all(sapply(gm, class) == "gribMessage"), TRUE)
 
   capture_output(grib_close(g))
 })
