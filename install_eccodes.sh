6b4e7cb4895b1efd7cdfe8a350f6b3908e90c409
Nathan Wendt <nathan.wendt@noaa.gov>
2022-09-12 14:42:21 -0500

    Testing github actions

--- install_eccodes.sh
+++ install_eccodes.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
 
-cd $HOME
+cd $GITHUB_WORKSPACE
 echo Staging dependency files to $(pwd)
 curl -o eccodes-${ECCODES_VERSION}-Source.tar.gz \
 		https://confluence.ecmwf.int/download/attachments/45757960/eccodes-${ECCODES_VERSION}-Source.tar.gz?api=v2
 tar -xzf eccodes-${ECCODES_VERSION}-Source.tar.gz
 cd eccodes-${ECCODES_VERSION}-Source
 
-mkdir build && cd build
+mkdir build
 
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-	AEC=ON
-else
-	AEC=OFF
-fi
+cmake . -B build -DCMAKE_BUILD_TYPE=Release \
+                 -DENABLE_NETCDF=OFF \
+                 -DENABLE_JPG=ON \
+                 -DENABLE_PNG=ON \
+                 -DENABLE_AEC=ON \
+                 -DENABLE_PYTHON=OFF \
+                 -DENABLE_FORTRAN=OFF \
+                 -DENABLE_EXAMPLES=OFF
 
-if [[ -n ${OPENJPEG_INCLUDE_DIR} ]]; then
-	cmake .. -DCMAKE_BUILD_TYPE=Release \
-	         -DENABLE_NETCDF=OFF \
-	         -DENABLE_JPG=ON \
-	         -DENABLE_PNG=ON \
-			 -DENABLE_AEC=${AEC} \
-	         -DENABLE_PYTHON=OFF \
-	         -DENABLE_FORTRAN=OFF \
-	         -DENABLE_EXAMPLES=OFF \
-	         -DOPENJPEG_INCLUDE_DIR=${OPENJPEG_INCLUDE_DIR}
-else
-	cmake .. -DCMAKE_BUILD_TYPE=Release \
-	         -DENABLE_NETCDF=OFF \
-	         -DENABLE_JPG=ON \
-	         -DENABLE_PNG=ON \
-			 -DENABLE_AEC=${AEC} \
-	         -DENABLE_PYTHON=OFF \
-	         -DENABLE_FORTRAN=OFF \
-	         -DENABLE_EXAMPLES=OFF
-fi
+cmake --build ./build
 
-make && sudo make install
-cd $TRAVIS_BUILD_DIR
+sudo cmake --install ./build
