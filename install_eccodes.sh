d6f5f1892cbdbb829e742a2a89be5c434cff6f77
Nathan Wendt <nathan.wendt@noaa.gov>
2022-09-12 16:33:15 -0500

    Fix action errors
    Make sure not to build from source in the package directory

--- install_eccodes.sh
+++ install_eccodes.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cd $GITHUB_WORKSPACE
-echo Staging dependency files to $(pwd)
+cd $HOME
+mkdir src
+cd src
+
+echo "Staging eccodes source files to $(pwd)"
 curl -o eccodes-${ECCODES_VERSION}-Source.tar.gz \
 		https://confluence.ecmwf.int/download/attachments/45757960/eccodes-${ECCODES_VERSION}-Source.tar.gz?api=v2
 tar -xzf eccodes-${ECCODES_VERSION}-Source.tar.gz
 cd eccodes-${ECCODES_VERSION}-Source
 
 mkdir build
-
 cmake . -B build -DCMAKE_BUILD_TYPE=Release \
                  -DENABLE_NETCDF=OFF \
                  -DENABLE_JPG=ON \
@@ -21,3 +23,5 @@ cmake . -B build -DCMAKE_BUILD_TYPE=Release \
 cmake --build ./build
 
 sudo cmake --install ./build
+
+cd $GITHUB_WORKSPACE
