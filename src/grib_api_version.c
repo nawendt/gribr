97ed881bf88f328d49261bf95620b2fcc1f9799e
Nathan Wendt <nathan.wendt@noaa.gov>
2023-04-03 07:12:47 -0500

    Fix snprintf argument

--- src/grib_api_version.c
+++ src/grib_api_version.c
@@ -8,14 +8,15 @@ SEXP gribr_api_version(void) {
   long major;
   long minor;
   long revision;
-  char string[15];
+  size_t slen = 15;
+  char string[slen];
   SEXP gribr_version;
 
   version = codes_get_api_version();
   major = version / 10000;
   minor = (version - major * 10000) / 100;
   revision = (version - major * 10000) - (minor * 100);
-  snprintf(string, "%ld.%ld.%ld\0", 15, major, minor, revision);
+  snprintf(string, "%ld.%ld.%ld\0", slen, major, minor, revision);
 
   gribr_version = PROTECT(mkString(string));
 
